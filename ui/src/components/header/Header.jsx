ec42968265eba0e474deba26aaeb77b95f886e47
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-10-13 21:23:23 +0530

    basic setup

---
+++ ui/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="logo"></div>
+      <div className="right-box">search icon</div>
+    </div>
+  );
+};
+
+export default Header;

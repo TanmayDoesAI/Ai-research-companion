32c30b511635a696bb015802a50c2e365a40ef93
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-10-16 21:48:36 +0530

    slight changes

--- ui/src/components/header/Header.jsx
+++ ui/src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import "./style.css";
 import "../../App.css";
 
@@ -14,7 +13,6 @@ const Header = () => {
         setIsScrolled(false);
       }
     };
-
     window.addEventListener("scroll", handleScroll);
 
     return () => {
@@ -27,13 +25,8 @@ const Header = () => {
       <div className={`header-container ${isScrolled ? "scrolled" : ""}`}>
         <div className="logo"><img src="./Images/logo.png" alt="" /></div>
         <div className="right-box">
-          <div className="search-container">search icon</div>
+          <div className="search-container"><i class="fas fa-magnifying-glass"></i></div>
         </div>
-      </div>
-      <div className="tagline-container">
-        <h2>AI-SAC: AI-enhanced Scholarly Assistance Companion</h2>
-        <h3>Elevating Your Research Journey with Intelligent Guidance.</h3>
-        <button className="get-started-button">Get Started</button>
       </div>
     </div>
   );
